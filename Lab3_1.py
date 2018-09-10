0db7f5da51b3c88d1c42234ffdfee952d1c32e46
seregamazur <serhiy.mazur0@gmail.com>
2018-09-10 13:08:42 +0300

    Lab3,Lab3_2

---
+++ Lab3_1.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+a = input("Input first number\n")
+b = input("Input second number\n")
+print("Yout sum is:",int(a)+int(b))
---
+++ Lab3_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("input the fahrenheit temp")
+fahrenheit =int(input())
+celsius = int((fahrenheit-32)/1.8)
+print("celsius =",celsius)
